132a4a98db478427762708c1978e655e45764a3e
xilapa <junior.dirceu@proton.me>
2023-08-30 20:09:57 -0300

    initial commit

---
+++ src/Worker/WorkerExample.cs
@@ -0,0 +1,20 @@
+namespace Worker;
+
+public class WorkerExample : BackgroundService
+{
+    private readonly ILogger<WorkerExample> _logger;
+
+    public WorkerExample(ILogger<WorkerExample> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+            await Task.Delay(1000, stoppingToken);
+        }
+    }
+}
